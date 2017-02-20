b241bace019cd6967725c92a18d8bba7be84416c
cmditch <cmditch@gmail.com>
2017-02-20 15:15:46 -0700

    Got delete working

--- routeList/MainFlattened.elm
+++ routeList/MainFlattened.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Html exposing (..)
-import Html.Attributes exposing (style, id)
+import Html.Attributes exposing (style, id, hidden, class, href)
 import Html.Events exposing (onClick)
+import List.Extra exposing (..)
 import Json.Decode exposing (..)
 import Json.Decode.Pipeline exposing (..)
 import Dict exposing (Dict)
@@ -12,6 +13,22 @@ import RoutesFlat
 (=>) : a -> b -> ( a, b )
 (=>) a b =
     ( a, b )
+
+
+groupBy : (v -> comparable) -> List v -> Dict comparable (List v)
+groupBy keyFromElem list =
+    let
+        reducer elem dict =
+            let
+                key =
+                    keyFromElem elem
+
+                values =
+                    Maybe.withDefault [] (Dict.get key dict)
+            in
+                Dict.insert key (List.append values [ elem ]) dict
+    in
+        List.foldl reducer Dict.empty list
 
 
 main =
@@ -93,14 +110,63 @@ init =
 
 
 type Msg
-    = DeleteRoute
+    = DeleteRoute Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        DeleteRoute ->
-            ( { model | lastMsg = toString msg }, Cmd.none )
+        DeleteRoute id ->
+            let
+                newRoutes =
+                    List.filter (\route -> route.id /= id) model.routes
+            in
+                ( { model | routes = newRoutes }, Cmd.none )
+
+
+viewWallTable : ( String, List Route ) -> Html Msg
+viewWallTable wallTuple =
+    let
+        ( wallName, routes ) =
+            wallTuple
+
+        bold =
+            ( "font-weight", "bold" )
+
+        xLarge =
+            ( "font-size", "x-large" )
+
+        padRight =
+            style [ ( "padding-right", "30px" ) ]
+
+        tableHead =
+            thead []
+                [ div [ style [ bold, xLarge ] ] [ text wallName ]
+                , tr [ style [ bold ] ]
+                    [ td [ padRight ] [ text "ID" ]
+                    , td [ padRight ] [ text "Name" ]
+                    , td [ padRight ] [ text "Grade" ]
+                    , td [ padRight ] [ text "Color" ]
+                    , td [] [ text "" ]
+                    ]
+                ]
+
+        tableBody =
+            tbody []
+                (List.map
+                    (\route ->
+                        tr []
+                            [ td [ padRight ] [ text <| toString route.id ]
+                            , td [ padRight ] [ text route.name ]
+                            , td [ padRight ] [ text route.tgrade ]
+                            , td [ padRight ] [ text route.color ]
+                            , td [] [ button [ onClick (DeleteRoute route.id), style [ ( "color", "orange" ) ] ] [ text "X" ] ]
+                            ]
+                    )
+                    routes
+                )
+    in
+        table [ style [ ( "padding-top", "50px" ) ] ] [ tableHead, tableBody ]
 
 
 
@@ -109,12 +175,12 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ div [] [ text <| toString <| List.length model.routes ]
-        , br [] []
-        , div []
-            (List.map
-                (\route -> ul [] [ li [] [ text route.name ] ])
-                model.routes
-            )
-        ]
+    let
+        wallTables =
+            (List.map (\wall -> viewWallTable wall) (Dict.toList <| groupBy .wallName model.routes))
+    in
+        div []
+            [ div [] [ text <| toString (List.length model.routes) ]
+            , a [] [ text "test" ]
+            , div [] wallTables
+            ]
